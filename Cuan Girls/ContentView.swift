2a1834a39944c5b4763946895924a5d6d8638c9a
celine1906 <reginaceline1906@gmail.com>
2025-07-18 14:49:05 +0700

    Initial Commit

---
+++ Cuan Girls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cuan Girls
+//
+//  Created by Regina Celine Adiwinata on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cuan Girls/Cuan_GirlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cuan_GirlsApp.swift
+//  Cuan Girls
+//
+//  Created by Regina Celine Adiwinata on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Cuan_GirlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
